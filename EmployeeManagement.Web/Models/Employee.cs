e1e24ba8060a81649121dd7ee47401ee32ee61f1
naresh <naresh@DESKTOP-C5G7JVM>
2025-05-17 10:34:37 -0700

    added the Employee model class.

---
+++ EmployeeManagement.Web/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace EmployeeManagement.Web.Models
+{
+    public class Employee
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public int Age { get;  set; }
+        public decimal Salary { get;  set; }
+        public string Department { get;  set; }
+
+        #region  CODE FOR ENCUSULATION 
+        //public string Department { get; private set; }
+        //public Employee(string name, int age, decimal salary, string department)
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    Salary = salary;
+        //    Department = department;
+        //}
+
+        //public void UpdateDetails(string name, int age, decimal salary, string department)
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    Salary = salary;
+        //    Department = department;
+        //}
+        #endregion
+    }
+
+}
